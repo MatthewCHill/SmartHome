723e0f09cefb2ed6c93d984020504e4f446b2815
Matthew Hill <matthill265@gmail.com>
2023-02-15 13:14:37 -0700

    Device Controller made with proper crud functions, properties and persistence

---
+++ SmartHome/ModelController/DeviceController.swift
@@ -0,0 +1,63 @@
+//
+//  DeviceController.swift
+//  SmartHome
+//
+//  Created by Matthew Hill on 2/15/23.
+//
+
+import Foundation
+
+class DeviceController {
+    
+    // MARK: - Properties
+    static let shared = DeviceController() // Singleton
+    
+    var devices: [Device] = [] // SSOT
+    
+    init() {
+        load()
+    }
+    
+    // MARK: - CRUD Functions
+    
+    func create(name: String) {
+        let device = Device(name: name)
+        devices.append(device)
+        save()
+        
+    }
+    func toggleButtonSwitched(whatDevice: Device) {
+        whatDevice.isOn.toggle()
+        save()
+    }
+    
+    // MARK: - Persistence
+    private var fileURL: URL? {
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {return nil}
+        let finalURL = documentsDirectory.appendingPathComponent("devices.json")
+        return finalURL
+    }
+    
+    func save() {
+        guard let url = fileURL else {return}
+        do {
+            let data = try JSONEncoder().encode(devices)
+            try data.write(to: url)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+        func load() {
+            guard let url = fileURL else {return}
+            do {
+                let data = try Data(contentsOf: url)
+                let devices = try JSONDecoder().decode([Device].self, from: data)
+                self.devices = devices
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
+    
+
