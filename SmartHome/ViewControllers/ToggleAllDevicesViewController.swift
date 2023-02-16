9747ea510d5fe4a24ca862c93f826ec79c609868
Matthew Hill <matthill265@gmail.com>
2023-02-16 13:36:21 -0700

    Last minute changes

---
+++ SmartHome/ViewControllers/ToggleAllDevicesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ToggleAllDevicesViewController.swift
+//  SmartHome
+//
+//  Created by Matthew Hill on 2/16/23.
+//
+
+import UIKit
+
+class ToggleAllDevicesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
